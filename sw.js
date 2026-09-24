4c2bebfdf665826ceb51becccc9fd394953c4731
mdaafaqueansari66-create <mdaafaqueansari66@gmail.com>
2026-09-25 00:30:38 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "study-tracker-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES);
+      })
+
+  );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response ||
+          fetch(event.request);
+
+      })
+
+  );
+
+});
